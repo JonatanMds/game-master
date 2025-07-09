587f66e7ad499e2892696af2da4473f801ca60d3
Jonatan Marques <marques.jonatands@gmail.com>
2025-07-09 11:05:44 -0300

    feature(card): improved design and added icons for shield, level, and HP
    - Enhanced the visual layout of the character card
    - Added a shield icon to represent defense
    - Included a level icon with centered number
    - Added an HP icon with a fantasy RPG style

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -3,6 +3,10 @@ import { CardProps } from "@/app/interface/Card"
 import Image from "next/image"
 import { useState } from "react"
 import HpInput from "../ui/hpInput"
+import ArmorClassIcon from "../icons/ArmorClassIcon"
+import HpIcon from "../icons/HpIcon"
+import LevelCharacterIcon from "../icons/LevelIcon"
+import DiceD20Icon from "../icons/DiceD20Icon"
 
 export default function Card(props: CardProps){
   const {name, ac, pt = 0, level} = props
@@ -21,12 +25,10 @@ export default function Card(props: CardProps){
   return(
       <div className="relative w-[320px] h-[360px]">
         <div className="z-10 absolute flex justify-between left-0 right-0">
-          <p className="top-0 left-0 bg-black bg-opacity-50 text-white font-bold p-1 m-0 text-2xl">
-            {level}
-          </p>
+          <LevelCharacterIcon levelCharacter={2}/>
           <div className="flex text-white font-bold m-0 text-xl gap-2">
-            <p className="flex items-center">{ac}</p>
-            <p className="text-center rounded-bl bg-[#ed4343] p-2">{hp}</p>
+            <ArmorClassIcon armorClass={ac}/>
+            <HpIcon hp={hp}/>
           </div>
         </div>
         <Image
@@ -35,14 +37,15 @@ export default function Card(props: CardProps){
           fill
           className="object-cover"
         />
-        <div className="absolute h-[180px] left-0 right-0 bottom-0 text-white font-bold p-1 m-0 text-xl flex-column items-center bg-gradient-to-b from-transparent to-black">
+        <div className="absolute h-[180px] left-0 right-0 bottom-0 text-white font-bold p-1 m-0 text-xl flex-column items-center bg-gradient-to-b from-transparent to-[#f3d796]">
           <p className="w-full flex justify-center">{name}</p>
           <div className="h-full flex justify-center items-center gap-4">
+            <DiceD20Icon DiceD20={10}/>
             <div className="flex gap-6">
               <HpInput operationName="Diminuir" onSubmit={subtractHp} bgColor="danger"/>
-              <HpInput operationName="Aumentar" onSubmit={addHp} maxHp={hp} bgColor="success"/>
+              <HpInput operationName="Aumentar" onSubmit={addHp} bgColor="success"/>
             </div>
-              <p className="p-4 bg-[#ed4343]">{hp}</p>
+              <HpIcon hp={hp}/>
           </div>
         </div>
       </div>
---
+++ src/app/components/icons/LevelIcon.tsx
@@ -0,0 +1,14 @@
+import { GiSwordsEmblem } from "react-icons/gi";
+
+interface IconProps {
+  levelCharacter: number
+}
+
+export default function LevelCharacterIcon({levelCharacter}:IconProps){
+  return(
+    <div className="relative w-[46px] h-[46px]">
+      <GiSwordsEmblem size={46} className="text-[#f3d796] drop-shadow-[0_1px_1px_rgba(255,255,255,0.4)]"/>
+      <p className="absolute inset-0 flex items-center justify-center top-1 text-white font-bold text-md drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]">{levelCharacter}</p>
+    </div>
+  )
+}
